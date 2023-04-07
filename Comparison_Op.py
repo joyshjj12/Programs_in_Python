0905e784ec26983a984abe526213732ae935e248
Joysh Jayaron Jose <joyshjj1234@gmail.com>
2023-04-07 10:49:42 +0530

    Comparison Program

---
+++ Comparison_Op.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number 1:"))
+b = int(input("Enter a number 2:"))
+
+if a > b:
+    print("Number 1 is greater than Number 2")
+    print(a, "is greater than", b)
+elif a < b:
+    print("Number 1 is less that Number 2")
+elif a == b:
+    print("Number 1 and Number 2 are equal")
+    
